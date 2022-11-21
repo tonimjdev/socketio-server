52614ccc8e049e61219c932d99870407d3aba561
tonimjdev <tonimdev@gmail.com>
2022-11-21 12:54:34 +0100

    Maquetación globos mensajes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 // settings
-app.set( 'port', process.env.PORT );
+app.set( 'port', process.env.PORT || 3000 );
 
 // static files
 app.use(express.static(path.join(__dirname, 'public')));
--- public/chat.js
+++ public/chat.js
@@ -2,28 +2,44 @@ const socket = io();
 
 // DOM elements
 let message = document.getElementById('message');
-let username = document.getElementById('username');
+// let username = document.getElementById('username');
 let btn = document.getElementById('send');
 let output = document.getElementById('output');
 let actions = document.getElementById('actions');
 
+let yoEscribo = false;
+
+let username = prompt('Hola, introduce tu nombre: ');
+console.log('username', username);
+
+
 btn.addEventListener('click', function() {
     socket.emit('chat:message', {
         message: message.value,
-        username: username.value
+        username: username
     });
+
+    yoEscribo = true;
+    message.value = "";
+
 });
 
 message.addEventListener('keypress', function() {
-    socket.emit('chat:typing', username.value);
+    socket.emit('chat:typing', username);
 })
 
 socket.on('chat:message', function(data) {
     actions.innerHTML = '';
-   output.innerHTML += `<p>
-    <strong>${data.username}</strong>: ${data.message}</p>`
+    yoEscribo ? output.innerHTML += `<p style="background: rgb(158, 255, 208); margin-left: 250px; border:none"><strong>${data.username}</strong>: ${data.message}</p>` :
+    output.innerHTML += `<p><strong>${data.username}</strong>: ${data.message}</p>`;
+    yoEscribo = false;
+
+   
 });
 
+
 socket.on('chat:typing', function (data) {
-    actions.innerHTML = `<p><em>${data} is typing a message...</em></p>`
+    actions.innerHTML = `<span><em>${data} is typing a message...</em></span>`
 } )
+
+
